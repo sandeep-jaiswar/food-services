833de69aded16c1c27251a84875c68c271282b7a
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2021-11-17 02:56:51 +0530

    refs#5 destructured modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { ProductModule } from './models/product/product.module';
+import { ProductModule } from './modules/product/product.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './models/user/user.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
